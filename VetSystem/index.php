24aa1a31b2859d6c49451ff84dbaca1a7cbe2b42
paintedlife <franticreader_11@yahoo.com>
2016-04-13 22:32:53 +0900

    Added files via upload

---
+++ VetSystem/index.php
@@ -0,0 +1,28 @@
+<html>
+	<head>
+	<link rel="stylesheet" href="css/general.css">
+	<link rel="stylesheet" href="css/input.css">
+	<link rel="stylesheet" href="css/label.css">
+	<link rel="stylesheet" href="css/header.css">
+	<title> Login </title>
+	</head>
+	<body>
+
+		<div id = "container">
+			<div id = "login">
+				<div class = "signin">
+				<div class = "header-bold"> Login: </div>
+				<form>
+					<label for = "username" class = "label-italic">Username: </label>
+					<input type = "text" name = "uname" id = "username" class = "input-small" required><p>
+					<label for = "password" class = "label-italic">Password: </label>
+					<input type = "password" name = "pword" id = "password" class = "input-small" required><p>
+					<a href = "#" class = "fp"> Forgot Password? </a>
+					<input type = "submit" name = "login" value = "Login" class = "input-button">
+				</form>
+				</div>
+			</div>
+		</div>
+
+	</body>
+</html>
